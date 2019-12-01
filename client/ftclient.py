323f292b5d6ec01f6ede199dd9bbb770d53ac69b
Ava Cordero <corderda@oregonstate.edu>
2019-12-01 11:10:08 -0800

    Modularized and implemented ftclient download_file() and list_dir().

--- client/ftclient.py
+++ client/ftclient.py
@@ -17,58 +17,84 @@ import signal
 import sys
 from socket import *
 
-#Function: conex_srv (Connect to Server)
+#Function: conex_srv(Connect to Server)
 #Description: Connects the client to the server.
 #Input: Server name and port to which to connect
 #Output: Tuple containing the client socket and server handle.
-def conex_srv (server_name, server_port):
+def conex_srv(server_name, server_port):
 # try:
 	# connect to the server
-	client_socket = socket (AF_INET, SOCK_STREAM) # create TCP socket
-	client_socket.connect ((server_name,server_port)) # connect to server
-	print ("Client connected to server '" + server_name + "' on port " + str(server_port) + "...")
+	client_socket = socket(AF_INET, SOCK_STREAM) # create TCP socket
+	client_socket.connect((server_name,server_port)) # connect to server
+	print("Client connected to server '" + server_name + "' on port " + str(server_port) + "...")
 
 	# returns the socket
 	return client_socket
 
-#Function: run_client (Run client)
+#Function: recv_message(Receive message)
+#Description: Receives a message from the other machine.
+#Input: Socket and maximum character length for the message.
+#Output: String including prompt and message.
+def recv_data(socket, message_max):
+	# receive the message back from server
+	# must be UTF-8
+	try:
+		sentence = socket.recv(message_max).decode ("UTF-8")
+		if sentence:
+			return sentence
+	except:
+		exit(1)
+
+#Function: run_client(Run client)
 #Description: Runs and maintains the client functionality.
 #Input: Client socket.
 #Output: None.
 # maintains the client file transfer functionality
-def run_client (client_socket):
+def run_client(client_socket):
 	# continuously prompt for a message until the message is "\q"
+	print("File transfer client starting...")
 	while 1:
-		msg_out = send_cmd (client_socket, ">", 500)
+		msg_out = send_cmd(client_socket, ">", 8192)
 
-#Function: send_message (Send message)
+#Function: send_message(Send message)
 #Description: Prompts for and sends a command to the other machine.
 #Input: Socket, prompt, and maximum character length for the message.
 #Output: String including message.
-def send_cmd (socket, prompt, message_max):
+def send_cmd(socket, prompt, message_max):
 	sentence = ""
-
 	# prompt user for a message that is no longer than the maximum
 	while len(sentence) > message_max or len(sentence) == 0:
-		sentence = input (prompt)
+		sentence = input(prompt)
 		if len(sentence) > message_max:
-			print ("Exceeded maximum characters allowed (" + str(message_max) + "), try again.")
+			print("Exceeded maximum characters allowed (" + str(message_max) + "), try again.")
 		elif sentence == "\\q": # if message is "\q", 
-			print ("Connection closed...")
-			socket.send (sentence.encode ("UTF-8")) # send the message
+			print("Connection closed...")
+			socket.send(sentence.encode("UTF-8")) # send the message
 			exit(0)
-		elif sentence.startswith("get"): # if client wants to get a file
-			print ("Downloading file: " + sentence.split()[1])
-			socket.send (sentence.encode ("UTF-8")) # send the message
+		elif sentence.startswith("get ") and len(sentence.split()) == 2: # if client wants to get a file
+			filename = sentence.split()[1]
+			
+			socket.send(sentence.encode("UTF-8")) # send the message
+			# receive and print the response
+			data = socket.recv(message_max).decode("UTF-8")
+			if data == "0":
+				print("File not found.")
+			else:
+				print("Downloading file: " + filename)
+				f = open(filename, "w+")
+				f.write(data)
+				print("File downloaded.")
 		elif sentence == "list": # if client wants to list directory contents
-			print ("Directory Contents: " + "")
-			socket.send (sentence.encode ("UTF-8")) # send the message
-			print(socket.recv (message_max).decode ("UTF-8"))
+			print("Directory Contents: " + "")
+			# send the message
+			socket.send(sentence.encode ("UTF-8"))
+			# receive and print the response
+			data = socket.recv(message_max).decode("UTF-8")
+			print(data)
 		else: # otherwise
-			print ("Not a valid command, try again.")
+			print("Not a valid command, try again.")
 
-
-	return (sentence) # must be UTF-8
+	return(sentence) # must be UTF-8
 
 # signal handler function
 def sig_handle(sig, frame):
@@ -76,26 +102,24 @@ def sig_handle(sig, frame):
 
 
 # Function: Main
-# Description: Validates arguments, then calls functions to configure the user, connect to the server, and run the file transfer client.
+# Description: Validates arguments, then calls functions to configure the connect to the server and run the file transfer client.
 # Input: Command line arguments.
 # Output: None.
 def main ():
 	# check for proper usage
 	if len(sys.argv) != 3:
-		print ("Error: Incorrect usage (ftclient.py servername portnumber).")
+		print("Error: Incorrect usage(ftclient.py servername portnumber).")
 		exit(1)
 
 	# assign host and port from args
 	server_name = sys.argv[1]
-	server_port = int (sys.argv[2])
-
-	print ("File transfer client starting ...")
+	server_port = int(sys.argv[2])
 
 	# connect to the server, get socket and handle
-	client_socket = conex_srv (server_name, server_port)
+	client_socket = conex_srv(server_name, server_port)
 
 	# run the file transfer client
-	run_client (client_socket) 
+	run_client(client_socket) 
 
 
 # assign signal handler function
--- server/ftserver.py
+++ server/ftserver.py
@@ -20,6 +20,53 @@ from socket import *
 
 
 
+def conex_client(client_socket):
+# try:
+	client_sock_def = client_socket.getsockname()
+	print("client_sock_def")
+	client_name = client_sock_def[0]
+	client_port = client_sock_def[1] + 1
+	# connect to the server
+	server_socket = socket(AF_INET, SOCK_STREAM) # create TCP socket
+	server_socket.connect((client_name,client_port)) # connect to server
+	print("Server connected to client '" + client_name + "' on port " + str(client_port) + "...")
+
+	# returns the socket
+	return server_socket
+
+#Function: download_file (Download file from server)
+#Description: Opens a data connection to the server and downloads the specified file.
+#Input: File name.
+#Output: None
+def download_file(file, socket):
+	data = ""
+	contents = os.listdir(".") # get dir contents
+	if file in contents: # find file
+		print("Sending file: " + file)
+		# conex_client(socket) # open data connection
+		f = open(file, "r+") # open and read file
+		data = f.read(8192)
+		socket.send(data.encode ("UTF-8")) #send data
+		print("File sent.")
+		# disconex_client(socket) # close data connection
+	else:
+		connection_socket.send ("0".encode ("UTF-8"))
+		print("File not found.")
+
+#Function: list_dir (List directory contents)
+#Description: Opens a data connection to the server, gets and prints directory contents.
+#Input: None.
+#Output: None.
+def list_dir(socket):
+	data = ""
+	contents = os.listdir(".")
+	for item in contents:
+		data += (item + "\n")
+	data = data[:-1]
+	print("Returning directory contents:")
+	print(data)
+	socket.send(data.encode ("UTF-8")) # send the message
+
 #Function: recv_message(Receive message)
 #Description: Receives a message from the other machine.
 #Input: Socket and maximum character length for the message.
@@ -34,11 +81,10 @@ def recv_cmd(socket, message_max):
 	except:
 		return
 
-#Function: run_client_srv(Run server-side client)
+#Function: run_client_srv (Run server-side client)
 #Description: Maintains the server-side file transfer functionality.
 #Input: Connection socket, server handle, and client handle.
-#Output: Returns 1 to start_srv to end the connection.
-# def run_client_srv(connection_socket, handle_srv, handle_cl):
+#Output: None.
 def run_client_srv(connection_socket):
 	# keep prompting until the message is "\q", or either machine sends a sigint
 	while 1:
@@ -52,28 +98,14 @@ def run_client_srv(connection_socket):
 				print("Connection closed...")
 				return 1
 			elif msg_in.startswith("get "):
-				file = msg_in.split()[1]
-				print("Sending file: " + file)
-				data = ""
-				contents = os.listdir(".")
-				if file in contents:
-					f = open(file, "r+")
-					data = f.read(8192)
-					connection_socket.send(data.encode ("UTF-8"))
-					print("File sent.")
-				else:
-					connection_socket.send ("0".encode ("UTF-8"))
-					print("File not found.")
+				# open new connection on port_num+1
+				# client_socket = conex_client(connection_socket)
+
+				file = msg_in.split()[1] # get filename
+				# download file
+				download_file(file, connection_socket)
 			elif msg_in == "list":
-				data = ""
-				contents = os.listdir(".")
-				for item in contents:
-					data += (item + "\n")
-				data = data[:-1]
-				print("Returning directory contents:")
-				print(data)
-				connection_socket.send(data.encode ("UTF-8")) # send the message
-					# send_dir(connection_socket, data)
+				list_dir(connection_socket)
 		else: # otherwise the connection has been closed by a sigint from either machine
 			print("Connection closed...")
 			return 1
@@ -87,7 +119,7 @@ def run_client_srv(connection_socket):
 
 #Function: start_srv(Start server)
 #Description: Starts and maintains the server functionality. Calls the run_client_srv function when a connection is established.
-#Input: Port and socket on which to run the server, and server handle.
+#Input: Port and socket on which to run the server.
 #Output: None.
 def start_srv(server_port, server_socket):
 	print("File server starting ...")
@@ -98,7 +130,6 @@ def start_srv(server_port, server_socket):
 		server_socket.listen(1) # wait for an incoming connection
 		connection_socket, addr = server_socket.accept() # accept and get socket info
 		print("Client connected to server on port " + str(server_port) + "...")
-		
 		# keep connection open until message is "\quit"
 		stop = 0
 		while stop is 0:
